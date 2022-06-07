93437a494b26f5010148fd6affd832ae513926d5
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2022-06-07 20:06:18 +0800

    WebApi.HttpBatchHandler 0 warnings

---
+++ Microshaoft.WebApiUtilities.NET.6.x/WebApi.HttpBatchHandler/Events/BatchRequestExecutedContext.cs
@@ -0,0 +1,34 @@
+﻿#if NETCOREAPP
+namespace Microshaoft.HttpBatchHandler.Events
+{
+    using System;
+    using Microsoft.AspNetCore.Http;
+    public class BatchRequestExecutedContext
+    {
+        /// <summary>
+        ///     Abort after this request?
+        /// </summary>
+        public bool Abort { get; set; }
+
+        /// <summary>
+        ///     Exception
+        /// </summary>
+        public Exception Exception { get; set; } = null!;
+
+        /// <summary>
+        ///     The individual HttpRequest
+        /// </summary>
+        public HttpRequest Request { get; set; } = null!;
+
+        /// <summary>
+        ///     The individual HttpResponse
+        /// </summary>
+        public HttpResponse Response { get; set; } = null!;
+
+        /// <summary>
+        ///     State
+        /// </summary>
+        public object State { get; set; } = null!;
+    }
+}
+#endif
---
+++ Microshaoft.WebApiUtilities.NET.6.x/WebApi.HttpBatchHandler/Events/BatchRequestPreparationContext.cs
@@ -0,0 +1,23 @@
+﻿#if NETCOREAPP
+namespace Microshaoft.HttpBatchHandler.Events
+{
+    using Microsoft.AspNetCore.Http.Features;
+    public class BatchRequestPreparationContext
+    {
+        /// <summary>
+        ///     Features which should be in the httpContext
+        /// </summary>
+        public IFeatureCollection Features { get; set; } = null!;
+
+        /// <summary>
+        ///     The individual request, prior to context creation
+        /// </summary>
+        public IHttpRequestFeature RequestFeature { get; set; } = null!;
+
+        /// <summary>
+        ///     State
+        /// </summary>
+        public object State { get; set; } = null!;
+    }
+}
+#endif
