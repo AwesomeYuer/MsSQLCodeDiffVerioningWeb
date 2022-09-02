de02ec3bb4752eb1a0dfd14690a1a2881295e414
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2022-09-02 18:46:48 +0800

    AssertHelper

--- Microshaoft.Common.Utilities.Net.6.x/Assert/AssertHelper.cs
+++ Microshaoft.Common.Utilities.Net.6.x/Assert/AssertHelper.cs
@@ -1,56 +1,91 @@
-﻿//namespace ChinaCPPMigTransLayer.MSTest.UnitTests
-//{
-//    public static class AssertHelper
-//    {
-//        public static void Throws<TExpectedException>
-//                                        (
-//                                            Action action
-//                                            , string expectedMessage = null!
-//                                        )
-//                                                where TExpectedException : Exception
-//        {
-//            void process(Exception exception)
-//            {
-//                if (!string.IsNullOrEmpty(expectedMessage))
-//                {
-//                    Assert
-//                        .AreEqual
-//                            (
-//                                expectedMessage
-//                                , exception.Message
-//                                , $"Expected exception with a message of '{expectedMessage}' but exception with message of '{exception.Message}' was thrown instead."
-//                            );
-//                }
-//            }
-//            try
-//            {
-//                action();
-//            }
-//            catch (TExpectedException expectedException)
-//            {
-//                Assert
-//                    .IsTrue
-//                        (
-//                            expectedException.GetType() == typeof(TExpectedException)
-//                        );
-//                process(expectedException);
-//                return;
-//            }
-//            catch (Exception exception)
-//            {
-//                Assert
-//                    .Fail
-//                        (
-//                            $"Expected exception of type {typeof(TExpectedException)} but type of {exception.GetType()} was thrown instead."
-//                        );
-//                process(exception);
-//                return;
-//            }
-//            Assert
-//                .Fail
-//                    (
-//                        $"Expected exception of type {typeof(TExpectedException)} but no exception was thrown."
-//                    );
-//        }
-//    }
-//}
+﻿# define Non_MSTEST_UnitTest
+# if MSTEST_UnitTest
+namespace ChinaCPPMigTransLayer.MSTest.UnitTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    public static class AssertHelper
+    {
+        private static void processExpectedExceptionMessage(Exception exception, string expectedExceptionMessage = null!)
+        {
+            if (!string.IsNullOrEmpty(expectedExceptionMessage))
+            {
+                Assert
+                    .AreEqual
+                        (
+                            expectedExceptionMessage
+                            , exception.Message
+                            , $@"Expected exception with a message of ""{expectedExceptionMessage}"" but exception with message of ""{exception.Message}"" was thrown instead."
+                        );
+            }
+        }
+        public static void Throws<TExpectedException>
+                                        (
+                                            Action action
+                                            , string expectedExceptionMessage = null!
+                                        )
+                                                where TExpectedException : Exception
+        {
+
+            try
+            {
+                action();
+            }
+            catch (TExpectedException expectedException)
+            {
+                Assert
+                    .IsTrue
+                        (
+                            expectedException.GetType() == typeof(TExpectedException)
+                        );
+                processExpectedExceptionMessage(expectedException, expectedExceptionMessage);
+                return;
+            }
+            catch (Exception exception)
+            {
+                Assert
+                    .Fail
+                        (
+                            $@"Expected exception of type ""{typeof(TExpectedException)}"" but type of ""{exception.GetType()}"" was thrown instead."
+                        );
+                processExpectedExceptionMessage(exception, expectedExceptionMessage);
+                return;
+            }
+            Assert
+                .Fail
+                    (
+                        $@"Expected exception of type ""{typeof(TExpectedException)}"" but no exception was thrown."
+                    );
+        }
+
+        public static void Throws
+                                (
+                                    Type expectedExceptionType
+                                    , Action action
+                                    , string expectedExceptionMessage = null!
+                                )
+
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception exception)
+            {
+                Assert
+                    .IsTrue
+                        (
+                            exception.GetType() == expectedExceptionType
+                            , $@"Expected exception of type ""{expectedExceptionType}"" but type of ""{exception.GetType()}"" was thrown instead."
+                        );
+                processExpectedExceptionMessage(exception, expectedExceptionMessage);
+                return;
+            }
+            Assert
+                .Fail
+                    (
+                        $@"Expected exception of type ""{expectedExceptionType}"" but no exception was thrown."
+                    );
+        }
+    }
+}
+#endif
