99f90b7748ca8ee0fa04b1d01f1301e0d6ddc206
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2023-03-09 21:24:42 +0800

    ZipHelper

---
+++ Microshaoft.Common.Utilities.Net.6.x/Zip/ZipHelper.cs
@@ -0,0 +1,213 @@
+﻿namespace Microshaoft;
+
+using System.IO.Compression;
+using System.Text;
+
+public static class ZipHelper
+{
+
+    public static async Task<Stream>
+                                CompressAsync<T>
+                                            (
+                                                this IEnumerable<T>
+                                                            @this
+                                                , Func<T, Task<(string, Stream)>>
+                                                            onUpdateEntryProcessFuncAsync
+                                                , Encoding?
+                                                            entryNameEncoding = null
+                                                , CompressionLevel
+                                                            entryCompressionLevelOnCreate = CompressionLevel.Optimal
+                                                , string?
+                                                            extractToDirectoryName = null
+                                            )
+    {
+        return
+            await
+                CompressAsync
+                        (
+                            @this
+                            , async (x) =>
+                            {
+                                (var entryName, var entryStream) = await onUpdateEntryProcessFuncAsync(x);
+                                return (false, true, entryName, entryStream);
+                            }
+                            , entryNameEncoding
+                            , entryCompressionLevelOnCreate
+                            , extractToDirectoryName
+                        );
+
+
+    }
+
+    public static async Task<Stream>
+                                    CompressAsync<T>
+                                                (
+                                                    this IEnumerable<T>
+                                                                @this
+                                                    , Func<T, Task<(bool, bool, string, Stream)>>
+                                                                onUpdateEntryProcessFuncAsync
+                                                    , Encoding?
+                                                                entryNameEncoding = null
+                                                    , CompressionLevel
+                                                                entryCompressionLevelOnCreate = CompressionLevel.Optimal
+                                                    , string?
+                                                                extractToDirectoryName = null
+                                                )
+    {
+        ZipArchive zipArchive = null!;
+        try
+        {
+            (
+                zipArchive
+                , Stream zipStream
+            )
+            = await ZipCompressAsync
+                            (
+                                @this
+                                , onUpdateEntryProcessFuncAsync
+                                , entryNameEncoding
+                                , entryCompressionLevelOnCreate
+                                , extractToDirectoryName
+                            );
+            return zipStream;
+        }
+        finally
+        {
+            if (zipArchive is not null)
+            {
+                zipArchive.Dispose();
+                //zipArchive = null!;
+            }
+        }
+    }
+
+    public static async Task<(ZipArchive, Stream)>
+                                ZipCompressAsync<T>
+                                            (
+                                                this IEnumerable<T>
+                                                            @this
+                                                , Func<T, Task<(string, Stream)>>
+                                                            onUpdateEntryProcessFuncAsync
+                                                , Encoding?
+                                                            entryNameEncoding = null
+                                                , CompressionLevel
+                                                            entryCompressionLevelOnCreate = CompressionLevel.Optimal
+                                                , string?
+                                                            extractToDirectoryName = null
+                                            )
+    {
+        return
+            await
+                ZipCompressAsync
+                    (
+                        @this
+                        , async (x) =>
+                        {
+                            (var entryName, var entryStream) = await onUpdateEntryProcessFuncAsync(x);
+                            return (false, true, entryName, entryStream);
+                        }
+                        , entryNameEncoding
+                        , entryCompressionLevelOnCreate
+                        , extractToDirectoryName
+                    );
+    }
+
+
+    public static async Task<(ZipArchive, Stream)>
+                                    ZipCompressAsync<T>
+                                                (
+                                                    this IEnumerable<T>
+                                                                @this
+                                                    , Func<T, Task<(bool, bool, string, Stream)>>
+                                                                onUpdateEntryProcessFuncAsync
+                                                    , Encoding?
+                                                                entryNameEncoding = null
+                                                    , CompressionLevel
+                                                                entryCompressionLevelOnCreate = CompressionLevel.Optimal
+                                                    , string?
+                                                                extractToDirectoryName = null
+                                                )
+    {
+        bool compressed = false;
+        ZipArchive zipArchive = null!;
+        MemoryStream zipStream = null!;
+        foreach (var item in @this)
+        {
+            (
+                bool needBreak
+                , bool needUpdateEntry
+                , string entryName
+                , Stream entryStream
+            )
+            = await onUpdateEntryProcessFuncAsync(item);
+
+            if
+                (
+                    needUpdateEntry
+                    &&
+                    entryStream != null
+                    &&
+                    !string.IsNullOrEmpty(entryName)
+                    &&
+                    !string.IsNullOrWhiteSpace(entryName)
+                )
+            {
+                if (zipArchive is null)
+                {
+                    zipStream = new MemoryStream();
+                    zipArchive = new ZipArchive
+                                            (
+                                                zipStream
+                                                , ZipArchiveMode.Update
+                                                , true
+                                                , entryNameEncoding
+                                            );
+                }
+
+                ZipArchiveEntry entry = zipArchive.GetEntry(entryName)!;
+                if (entry == null)
+                {
+                    entry = zipArchive.CreateEntry(entryName, entryCompressionLevelOnCreate);
+                }
+
+                using var entryUpdateStream = entry.Open();
+                await entryStream.CopyToAsync(entryUpdateStream);
+                entryUpdateStream.Close();
+                entry = null!;
+
+                if (!compressed)
+                {
+                    compressed = true;
+                }
+            }
+            if (needBreak)
+            {
+                break;
+            }
+        }
+        if
+            (
+                compressed
+            //&&
+            //zipStream is not null
+            )
+        {
+            if
+                (
+                    !string.IsNullOrEmpty(extractToDirectoryName)
+                    &&
+                    !string.IsNullOrWhiteSpace(extractToDirectoryName)
+                )
+            {
+                if (!Directory.Exists(extractToDirectoryName))
+                {
+                    Directory.CreateDirectory(extractToDirectoryName);
+                }
+                zipArchive.ExtractToDirectory(extractToDirectoryName, true);
+            }
+            zipStream.Position = 0;
+        }
+        return
+            (zipArchive, zipStream);
+    }
+}
