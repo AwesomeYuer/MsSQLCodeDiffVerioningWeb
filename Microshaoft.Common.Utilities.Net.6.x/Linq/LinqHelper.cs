b608335546f4aecd61d37934001eb57bc52f1e3c
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2023-03-13 00:39:09 +0800

    LinqHelper.cs

---
+++ Microshaoft.Common.Utilities.Net.6.x/Linq/LinqHelper.cs
@@ -0,0 +1,114 @@
+﻿public static class LinqHelper
+{
+    public static void ForEach<T>(this IEnumerable<T> @this, Func<int, T, bool> eachProcessFunc)
+    {
+        var i = 0;
+        foreach (T x in @this)
+        {
+            i++;
+            if (eachProcessFunc(i, x))
+            {
+                break;
+            }
+        }
+    }
+
+    public static IEnumerable<T>
+                                TakeTopFirst<T>
+                                        (
+                                            this IEnumerable<T> @this
+                                            , Func<int, T, bool> predict
+                                        )
+    {
+        var i = 0;
+        foreach (T x in @this)
+        {
+            i++;
+            yield return x;
+            if (predict(i, x))
+            {
+                break;
+            }
+        }
+    }
+
+    public static IEnumerable<T>
+                            TakeTopFirst<T>
+                                    (
+                                        this IEnumerable<T> @this
+                                        , Func<int, T, Task<bool>> predictAsync
+                                    )
+    {
+        var i = 0;
+        foreach (T x in @this)
+        {
+            i++;
+            yield return x;
+            bool b;
+            if (b = predictAsync(i, x).Result)
+            {
+                break;
+            }
+        }
+    }
+
+    public static async IAsyncEnumerable<T>
+                                TakeTopFirstAsync<T>
+                                        (
+                                            this IEnumerable<T> @this
+                                            , Func<int, T, Task<bool>> predictAsync
+                                        )
+    {
+        var i = 0;
+        foreach (T x in @this)
+        {
+            i++;
+            yield return x;
+            bool b;
+            if (b = await predictAsync(i, x))
+            {
+                break;
+            }
+        }
+    }
+
+    public static async IAsyncEnumerable<T> 
+                                TakeTopFirstAsync<T>
+                                        (
+                                            this IAsyncEnumerable<T> @this
+                                            , Func<int, T, Task<bool>> predictAsync
+                                        )
+    {
+        var i = 0;
+        await foreach (T x in @this)
+        {
+            i++;
+            yield return x;
+            bool b;
+            if (b = await predictAsync(i, x))
+            {
+                break;
+            }
+        }
+    }
+
+    public static async IAsyncEnumerable<T>
+                            TakeTopFirstAsync<T>
+                                    (
+                                        this IAsyncEnumerable<T> @this
+                                        , Func<int, T, bool> predict
+                                    )
+    {
+        var i = 0;
+        await foreach (T x in @this)
+        {
+            i++;
+            yield return x;
+            bool b;
+            if (b = predict(i, x))
+            {
+                break;
+            }
+        }
+    }
+}
