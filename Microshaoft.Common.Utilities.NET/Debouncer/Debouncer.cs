7d8bc597658fa5b3589879c2c6762d0c2e6f2823
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2023-05-23 09:18:36 +0800

    1

---
+++ Microshaoft.Common.Utilities.NET/Debouncer/Debouncer.cs
@@ -0,0 +1,57 @@
+﻿namespace Microshaoft;
+
+public class Debouncer<T> : IDisposable
+{
+    private readonly CancellationTokenSource cts = new CancellationTokenSource();
+    private readonly TimeSpan waitTime;
+    private int counter;
+
+    public Debouncer(TimeSpan? waitTime = null)
+    {
+        waitTime = waitTime ?? TimeSpan.FromSeconds(3);
+    }
+
+    public void Debounce(Action<T> action, T state)
+    {
+        var current = Interlocked.Increment(ref counter);
+
+        Task.Delay(waitTime).ContinueWith(task =>
+        {
+            // Is this the last task that was queued?
+            if (current == counter && !cts.IsCancellationRequested)
+                action(state);
+#if NETSTANDARD2_0
+              task.Dispose();
+#endif
+
+        }, cts.Token);
+    }
+
+    public void Dispose()
+    {
+        cts.Cancel();
+    }
+}
+
+public class Debouncer : Debouncer<object>
+{
+    public Debouncer(TimeSpan? waitTime = null) : base(waitTime)
+    {
+    }
+
+    public void Debounce(Action action)
+    {
+        Debounce
+            (
+                (x) =>
+                {
+                    action();
+                }
+                , null!
+            );
+    }
+    //public void Dispose()
+    //{
+    //    Dispose();
+    //}
+}
